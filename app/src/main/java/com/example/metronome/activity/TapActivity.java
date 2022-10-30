0b9a237ac6b9378f0d3bb503588183520bb31754
arturoo404 <starwars99999s@gmail.com>
2022-10-30 23:48:10 +0100

    Menu for activites

--- app/src/main/java/com/example/metronome/activity/TapActivity.java
+++ app/src/main/java/com/example/metronome/activity/TapActivity.java
@@ -1,21 +1,33 @@
-package com.example.metronome;
+package com.example.metronome.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.metronome.activity.TapActivity;
+import com.example.metronome.MainActivity;
+import com.example.metronome.R;
+import com.example.metronome.button_listener.TapButton;
 
-public class MainActivity extends AppCompatActivity {
+public class TapActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tap);
+        tapInit();
     }
+
+    private void tapInit(){
+        Button button = findViewById(R.id.tap_button);
+        button.setOnClickListener(new TapButton(findViewById(R.id.tempo)));
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -26,9 +38,8 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        if (item.getItemId() == R.id.tap_tempo_option) {
-            Intent intent = new Intent(this, TapActivity.class);
+        if (item.getItemId() == R.id.metronome_option) {
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
