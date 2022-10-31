c82499730436a61051af181a7601d4e551f2fd60
arturoo404 <starwars99999s@gmail.com>
2022-10-31 23:15:00 +0100

    Design metronome layout

--- app/src/main/java/com/example/metronome/MainActivity.java
+++ app/src/main/java/com/example/metronome/MainActivity.java
@@ -5,16 +5,27 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.metronome.activity.TapActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NumberPicker numberPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberPickerInit();
+    }
+
+
+    private void numberPickerInit(){
+        numberPicker = findViewById(R.id.tempo_picker);
+        numberPicker.setMinValue(40);
+        numberPicker.setMaxValue(250);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
